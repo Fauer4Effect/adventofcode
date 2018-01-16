ec996f346ac4abf8f7e28c4b836b9cbd7603025e
Fauer4Effect <kyle.fauer@gmail.com>
2018-01-16 17:15:33 -0500

    add day24 solution

--- 2017/day24.py
+++ 2017/day24.py
@@ -5,40 +5,29 @@ Python solution to advent of code day 24
 
 from collections import defaultdict, deque
 
-def part1():
+def solve():
     """
     answer should be
     """
     with open("24_1_in.txt", "r") as my_input:
-        all_pipes = map(str.strip, my_input.readlines())
-    pipes = defaultdict(list)
+        all_pipes = [map(int, line.strip().split('/')) for line in my_input]
+    lengths = []
+    def find_next(port, remaining, cur_total, length):
+        lengths.append([length, cur_total])
+        for next_pipe in remaining:
+            x, y = next_pipe
+            if x == port or y == port:
+                new_remaining = [new_pipe for new_pipe in remaining if new_pipe != next_pipe]
+                find_next(x if y == port else y, new_remaining, cur_total + sum(next_pipe), length + 1) 
     for pipe in all_pipes:
-        pipe1 = pipe.split('/')
-        pipes[pipe1[0]].append(pipe)
-        pipes[pipe1[1]] .append(pipe)
-    explore = deque()
-    need = '0'
-    explore.extend(pipes[need])
-    paths = []
-    path = []
-    used = set()
-    while explore:
-        cur_pipe = explore.pop()
-        if cur_pipe not in used:
-            print path
-            pipe1 = cur_pipe.split('/')
-            if pipe1[0] == '0' or pipe1[1] == '0':
-                need = '0'
-                path = []
-                used = set()
-            if pipe1[0] == need:
-                need = pipe1[1]
-            elif pipe1[1] == need:
-                need = pipe1[0]
-            explore.extend(pipes[need])
-            path.append(cur_pipe)
-            paths.append(path)
-            used.update(path)
+        x, y = pipe
+        if x == 0 or y == 0:
+            remaining = [new_pipe for new_pipe in all_pipes if new_pipe != pipe]
+            find_next(x if y == 0 else y, remaining, sum(pipe), 1)
+    print "part1", max([length[1] for length in lengths])
+
+    longest = max([length[0] for length in lengths])
+    print "part2", max([length[1] for length in lengths if length[0] == longest])
 
 if __name__ == "__main__":
-    print "part1", part1()
+    solve()
