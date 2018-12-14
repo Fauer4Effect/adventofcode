a250e0870596fad38071a86e6f1f54d45b84456e
fauer4effect <kyle.fauer@gmail.com>
2018-12-14 10:16:56 -0500

    day10

--- 2018/day10.py
+++ 2018/day10.py
@@ -5,8 +5,10 @@ Python solution to advent of code day 10
 
 import re
 
-def part1():
+def solve():
     """
+    THIS IS WAY FASTER IF YOU USE pypy
+
     You know you should stop when the rectangle encompassing all the points starts
     to get bigger again. At this point the points are moving farther away from each other.
     this makes sense because when they form words they should be organized and pretty
@@ -17,26 +19,37 @@ def part1():
     with open("10_1_in.txt", "r") as my_input:
         my_input = my_input.readlines()
 
-    positions = [0 for _ in range(len(my_input))]
-    velocities = [0 for _ in range(len(my_input))]
+    get_nums = re.compile(r'-?\d+')  # save some time by only doing this once
+    data = [list(map(int, get_nums.findall(line))) for line in my_input]
 
-    get_nums = re.compile(r"-*[0-9]+")  # save some time by only doing this once
-    i = 0
-    for line in my_input:
-        # we verified this will unpack correctly by checking that all lengths were 4 first
-        pos_x, pos_y, vel_x, vel_y = list(map(int, get_nums.findall(line)))
-        positions[i] = (pos_x, pos_y)
-        velocities[i] = (vel_x, vel_y)
-        i += 1
+    bounding_boxes = []
+    # arbitrarily guess that we will find the pattern by then
+    for sec in range(20000):
+        minx, maxx, miny, maxy = 10000, 0, 10000, 0
+        for x, y, vx, vy in data:
+            minx = min(minx, x + sec * vx)
+            maxx = max(maxx, x + sec * vx)
+            miny = min(miny, y + sec * vy)
+            maxy = max(maxy, y + sec * vy)
+        bounding_boxes.append([maxx, minx, maxy, miny])
 
-    # TODO the actual hard stuff
+    # find the minimum bounding box achieved in that time
+    min_bounding = min(maxx - minx + maxy - miny for maxx, minx, maxy, miny in bounding_boxes)
+    # find what second aligns with that minimum bounding box
+    for sec, (maxx, minx, maxy , miny) in enumerate(bounding_boxes):
+        if min_bounding == maxx - minx + maxy - miny:
+            break
+    grid = [[' ' for _ in range(minx, maxx+1)] for _ in range(miny, maxy+1)]
+    for (x, y, vx, vy) in data:
+        grid[y + sec * vy - miny][x + sec * vx - minx] = '#'
 
-    return None
+    ans = []
+    for row in grid:
+        ans.append(''.join(row))
 
-def part2():
-
-    return None
+    return '\n'.join(ans), sec
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    string, time = solve()
+    print(string)
+    print(time)
