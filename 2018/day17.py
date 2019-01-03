5e32cfe271db766d052dc55b9d57f410e1415324
fauer4effect <kyle.fauer@gmail.com>
2019-01-03 12:09:16 -0500

    day17

--- 2018/day17.py
+++ 2018/day17.py
@@ -3,13 +3,41 @@ Author: Kyle Fauerbach
 Python solution to advent of code day 17
 """
 
-from collections import deque
+from collections import defaultdict, deque
+import operator
 import re
 
-def part1():
+def supported(cur_pos, points):
+    x, y = cur_pos
+    while points[(x, y)] != '#':
+        if points[(x, y+1)] != '#' and points[(x, y+1)] != '~':
+            return False
+        x -= 1
+    x, y = cur_pos
+    while points[(x, y)] != '#':
+        if points[(x, y+1)] != '#' and points[(x, y+1)] != '~':
+            return False
+        x += 1
+    return True
+
+def flowing(cur_pos, points):
+    x, y = cur_pos
+    while points[(x, y)] != '#':
+        if points[(x, y)] == '|':
+            return True
+        x -= 1
+    x, y = cur_pos
+    while points[(x, y)] != '#':
+        if points[(x, y)] == '|':
+            return True
+        x += 1
+    return False
+
+def run_simulation():
+    points = defaultdict(lambda: ".")
+
     with open("17_1_in.txt", "r") as my_input:
         my_input = my_input.readlines()
-    clay = set()
     min_y = 100000000
     max_y = 0
     for line in my_input:
@@ -21,40 +49,96 @@ def part1():
             min_y = min(min_y, y)
             max_y = max(max_y, y)
             for x2 in range(lo, hi+1):
-                clay.add((x2, y))
+                points[(x2, y)] = '#'
         elif '..' in y:
             x = int(x)
             lo, hi = list(map(int, y.split('..')))
             min_y = min(min_y, lo)
             max_y = max(max_y, hi)
             for y2 in range(lo, hi+1):
-                clay.add((x, y2))
+                points[(x, y2)] = '#'
         else:
             x = int(x)
             y = int(y)
-            clay.add((x, y))
+            points[(x, y)] = '#'
             min_y = min(min_y, y)
             max_y = max(max_y, y)
+    check = deque()
 
-    print(min_y, max_y)
-    cur_pos = (500, 0)
+    points[(500, min_y)] = '|'
+    check.append((500, min_y+1))
 
-    while 1:
-        # if you can go down then go down
-        # if beneath you is clay or settled water and between to walls you can settle
-        # if it's not then you touch but don't settle and you continue down
-        # if you can't go down and beneath you is settled or clay you can spread either direction
-        # until you hit a wall or are able to continue down
+    while check:
+        modified = False
+        x, y = check.pop()
+        if (x, y) == (500, 2):
+            print(supported((x, y), points))
+        if points[(x, y)] == '#':
+            continue
 
-        break
+        if points[(x, y-1)] == '|' and points[(x, y)] == '.':
+            points[(x, y)] = '|'
+            modified = True
+        if points[(x-1, y)] == '~' and points[(x, y)] != '~':
+            points[(x, y)] = '~'
+            modified = True
+        if points[(x+1, y)] == '~' and points[(x, y)] != '~':
+            points[(x, y)] = '~'
+            modified = True
+        if points[(x, y-1)] == '~' and points[(x, y)] != '~':
+            points[(x, y)] = '~'
+            modified = True
+        if points[(x, y)] == '|' and supported((x, y), points):
+            points[(x, y)] = '~'
+            modified = True
+        if points[(x, y)] == '.' and supported((x, y), points) and flowing((x, y), points):
+            points[(x, y)] = '~'
+            modified = True
+        if points[(x, y)] == '|' and (points[(x, y+1)] == '#' or points[(x, y+1)] == '~'):
+            if points[(x-1, y)] != '#' and points[(x-1, y)] != '|':
+                points[(x-1, y)] = '|'
+                modified = True
+                check.append((x-2, y))
+                check.append((x, y))
+                if y+1 <= max_y:
+                    check.append((x-1, y+1))
+                check.append((x-1, y-1))
+                check.append((x-1, y))
+            if points[(x+1, y)] != '#' and points[(x+1, y)] != '|':
+                points[(x+1, y)] = '|'
+                modified = True
+                check.append((x, y))
+                check.append((x+2, y))
+                if y+1 <= max_y:
+                    check.append((x+1, y+1))
+                check.append((x+1, y-1))
+                check.append((x+1, y))
 
+        if modified:
+            check.append((x-1, y))
+            check.append((x+1, y))
+            if y+1 <= max_y:
+                check.append((x, y+1))
+            check.append((x, y-1))
+            check.append((x, y))
 
-    return None
+    return points
 
-def part2():
+def part1(points):
+    water = 0
+    for point, value in points.items():
+        if value == '|' or value == '~':
+            water += 1
+    return water
 
-    return None
+def part2(points):
+    water = 0
+    for point, value in points.items():
+        if value == '~':
+            water += 1
+    return water
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    points = run_simulation()
+    print(part1(points))
+    print(part2(points))
